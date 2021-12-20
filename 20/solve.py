3846cc2d8b43af76d2e292f0ff5801f4b7ecac17
davidot <david.tuin@gmail.com>
2021-12-20 11:05:26 +0100

    Day 20: Python in ~40 minutes, does take 10s for input
    Not very hard just a kind of game of life thing with flickering
    points to infinity. Can probably speed this up by not doing things
    in string but converting binary ourself but it is fast enough.

---
+++ 20/solve.py
@@ -0,0 +1,109 @@
+#!/bin/python3
+
+import sys
+
+if len(sys.argv) < 2:
+    print('Provide filename')
+    exit(0)
+    
+filename = sys.argv[1]
+
+enhancement = ""
+in_image = []
+
+with open(filename) as file:
+    first = True
+    for l in file:
+        l = l.strip()
+        if first:
+            enhancement = l
+            first = False
+            continue
+        
+        if l == '':
+            continue
+        
+        in_image.append(l.replace('#', '1').replace('.', '0'))
+
+if len(in_image) != len(in_image[0]):
+    print("Non square not sure if this handles that")
+
+def expand(inp, fill, amount=3):
+    if len(inp) != len(inp[0]):
+        print("expand only works on already square stuff")
+        exit(-2)
+        
+    new_size = len(inp) + 2 * amount
+    
+    img = [[fill for _ in range(new_size)] for _ in range(new_size)]
+
+    for x, l in enumerate(inp):
+        for y, c in enumerate(l):
+            img[amount + x][amount + y] = c
+            
+    return img
+    
+
+image = expand(in_image, '0')
+
+enhancement = enhancement.replace('#', '1').replace('.', '0')
+
+outside_val = '0'
+
+def iteration(in_image, outside_val):
+    if outside_val == '0':
+        new_outside_val = enhancement[0]
+    else:
+        new_outside_val = enhancement[-1]
+
+    img_size = len(in_image)
+
+    out_image = [['x' for _ in range(img_size)] for _ in range(img_size)]
+
+    def get_c(x, y):
+        if x < 0 or x >= img_size or y < 0 or y >= img_size:
+            return outside_val
+        return in_image[x][y]
+
+    for x in range(img_size):
+        for y in range(img_size):
+            bin_n = ''
+            for i in range(x - 1, x + 2):
+                for j in range(y - 1, y + 2):
+                    bin_n += get_c(i, j)
+            
+            out_image[x][y] = enhancement[int(bin_n, 2)]
+    
+    
+    return out_image, new_outside_val
+
+iters = 50
+
+
+
+def count_lights(image, outside):
+    if outside == '1':
+        print("Outside is on so infinity lights are on????")
+        exit(-2)
+        
+    count = 0
+    for x in range(len(image)):
+        for y in range(len(image)):
+            if image[x][y] == '1':
+                count += 1
+    
+    return count
+    
+
+for i in range(iters):
+    image, outside_val = iteration(image, outside_val)
+    image = expand(image, outside_val)
+    
+    if i == 1:
+        print('#Part 1 after 2 iters ligths on', count_lights(image, outside_val))
+    
+    #print('\n'.join(''.join('.' if c == '0' else '#' for c in l) for l in image))
+    #print(outside_val)
+
+print('#Part 2 after 50 iters ligths on', count_lights(image, outside_val))
+
